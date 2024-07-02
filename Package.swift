c5ac14241cd56de1ae0a1eef8d56fc0884284d1b
Igor <igorshelopaev@gmail.com>
2024-07-02 18:32:55 +0200

    update

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "sharelink-swiftui",
+    name: "sharelink-for-swiftui",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "sharelink-swiftui",
-            targets: ["sharelink-swiftui"]),
+            name: "sharelink-for-swiftui",
+            targets: ["sharelink-for-swiftui"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "sharelink-swiftui",
-            dependencies: []),
-        .testTarget(
-            name: "sharelink-swiftuiTests",
-            dependencies: ["sharelink-swiftui"]),
+            name: "sharelink-for-swiftui",
+            dependencies: [])
     ]
 )
