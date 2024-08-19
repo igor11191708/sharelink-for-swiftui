1eb8a44fbca92985dfb577745b7b3bcfabaafb02
Igor <igorshelopaev@gmail.com>
2024-08-19 14:06:42 +0200

    Update ShareLinkButtonTests.swift

--- Tests/sharelinkTests/ShareLinkButtonTests.swift
+++ Tests/sharelinkTests/ShareLinkButtonTests.swift
@@ -9,16 +9,17 @@ import XCTest
 import SwiftUI
 @testable import sharelink_for_swiftui
 
+
 final class ShareLinkButtonTests: XCTestCase {
 
-    func testShareText() {
+    @MainActor func testShareText() {
         let shareLinkButton = ShareLinkButton(item: "Hello, world!") {
             Text("Share Text")
         }
         XCTAssertNotNil(shareLinkButton)
     }
 
-    func testShareURL() {
+    @MainActor func testShareURL() {
         let url = URL(string: "https://example.com")!
         let shareLinkButton = ShareLinkButton(item: url) {
             Text("Share URL")
@@ -26,7 +27,7 @@ final class ShareLinkButtonTests: XCTestCase {
         XCTAssertNotNil(shareLinkButton)
     }
 
-    func testShareImage() {
+    @MainActor func testShareImage() {
         let image = UIImage(systemName: "photo")!
         let shareLinkButton = ShareLinkButton(item: image) {
             Image(systemName: "photo")
@@ -34,18 +35,12 @@ final class ShareLinkButtonTests: XCTestCase {
         XCTAssertNotNil(shareLinkButton)
     }
 
-    func testCustomActivity() {
+    @MainActor func testCustomActivity() {
         let customActivity = UIActivity()
         let shareLinkButton = ShareLinkButton(item: "Custom Activity", applicationActivities: [customActivity]) {
             Text("Share Custom Activity")
         }
         XCTAssertNotNil(shareLinkButton)
     }
-    
-    static var allTests = [
-        ("testShareText", testShareText),
-        ("testShareURL", testShareURL),
-        ("testShareImage", testShareImage),
-        ("testCustomActivity", testCustomActivity),
-    ]
+
 }
